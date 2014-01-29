0828f378d1945dd728e9547e09c8fc00266769d9
leomoty <leomoty@gmail.com>
2014-01-29 18:08:53 -0200

    FixedNavbar: Converted inline css to css block

--- steamgifts-plus-v2.js
+++ steamgifts-plus-v2.js
@@ -7,7 +7,7 @@ var SGPlusV2 = {
     },
     generateStyles: function () {
     	var styles = document.head.appendChild(document.createElement('style'));
-    	styles.innerHTML = '.short .markdown{overflow:hidden;max-height:100px;position:relative}.less__beautify{position:absolute;width:100%;bottom:0;display:none;background:-webkit-gradient(linear,left top,left bottom,from(rgba(240,242,245,0)),to(rgba(240,242,245,1)));background:-moz-linear-gradient(top,rgba(240,242,245,0),rgba(240,242,245,1));background:linear-gradient(top,rgba(240,242,245,0),rgba(240,242,245,1));height:20px}.less__beautify.sub{background:-webkit-gradient(linear,left top,left bottom,from(rgba(243,244,247,0)),to(rgba(243,244,247,1)));background:-moz-linear-gradient(top,rgba(243,244,247,0),rgba(243,244,247,1));background:linear-gradient(top,rgba(243,244,247,0),rgba(243,244,247,1))}.short .less__beautify{display:block}.comment_more{display:none}.short .comment_more{display:block}.short .comment_less{display:none}';
+    	styles.innerHTML = '.short .markdown{overflow:hidden;max-height:100px;position:relative}.less__beautify{position:absolute;width:100%;bottom:0;display:none;background:-webkit-gradient(linear,left top,left bottom,from(rgba(240,242,245,0)),to(rgba(240,242,245,1)));background:-moz-linear-gradient(top,rgba(240,242,245,0),rgba(240,242,245,1));background:linear-gradient(top,rgba(240,242,245,0),rgba(240,242,245,1));height:20px}.less__beautify.sub{background:-webkit-gradient(linear,left top,left bottom,from(rgba(243,244,247,0)),to(rgba(243,244,247,1)));background:-moz-linear-gradient(top,rgba(243,244,247,0),rgba(243,244,247,1));background:linear-gradient(top,rgba(243,244,247,0),rgba(243,244,247,1))}.short .less__beautify{display:block}.comment_more{display:none}.short .comment_more{display:block}.short .comment_less{display:none}body{margin-top:39px}header{margin-left:-25px;position:fixed;top:0;width:100%;z-index:1}.navbar_fixed{padding:0 25}';
     },
     generateGridview: function () {
         var container = document.createElement('div');
@@ -55,19 +55,10 @@ var SGPlusV2 = {
         });
     },
     generateFixedNavbar: function () {
-        $('body').css('margin-top', '39px');
-        $('header').css({
-            marginLeft: '-25px',
-            position: 'fixed',
-            top: 0,
-            width: '100%',
-            zIndex: 1
-        });
-
         var nav = $('header').html();
         $('nav').remove();
-        $('header').html('<div id="navbar_fixed" style="padding:0 25px;"></div>');
-        $('#navbar_fixed').html(nav);
+        $('header').html('<div class="navbar_fixed"></div>');
+        $('.navbar_fixed').html(nav);
 
         $('nav .nav__button--is-dropdown-arrow').click(function () {
             var active = $(this).hasClass('is-selected');
