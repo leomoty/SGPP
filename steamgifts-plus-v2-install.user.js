08e5faf729c3b4ff98d51bf18e18fbb9d2b45b85
leomoty <leomoty@gmail.com>
2014-02-02 14:26:06 -0200

    Initial commit to add localstorage support
    Adding option to support localstorage or chrome.storage.sync if
    possible.

--- steamgifts-plus-v2-install.user.js
+++ steamgifts-plus-v2-install.user.js
@@ -2,7 +2,7 @@
 // @name            SteamGifts Plus v2
 // @version         2.0.0 - BETA
 // @namespace       steamgiftsplusv2
-// @description     Adds various features.
+// @description     Initial Beta Release.
 // @copyright       2014 leomoty <leomoty@gmail.com>
 // @license         MIT; http://en.wikipedia.org/wiki/Mit_license
 // @match           http://www.steamsocial.com/*
@@ -12,6 +12,6 @@
 if (window.top == window) {
     var SGPscript = document.createElement('script');
     SGPscript.type = 'text/javascript';
-    SGPscript.src = 'https://raw2.github.com/leomoty/SGV2-/master/steamgifts-plus-v2.js?v=' + new Date().getTime();
+    SGPscript.src = 'https://raw2.github.com/leomoty/SGV2-/dev/steamgifts-plus-v2.js?v=' + new Date().getTime();
     document.body.appendChild(SGPscript);
 }
