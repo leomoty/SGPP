be3832717ca2457d00bd496d7e9931c2dc7f4352
leomoty <leomoty@gmail.com>
2014-03-10 22:56:42 -0300

    Changed to Chrome.storage.sync since LocalStorage is meh

--- Google Chrome/SGPlusV2ChromeExtension/options.js
+++ Google Chrome/SGPlusV2ChromeExtension/options.js
@@ -2,35 +2,29 @@
         // Saves options to localStorage.
         function save_options() {
 
-            var gridview = document.getElementById('gridview');
-            var shorten_comments = document.getElementById('shorten_comments');
-            var scrolling_sidebar = document.getElementById('scrolling_sidebar');
-            var fixed_navbar = document.getElementById('fixed_navbar');
+            var gridview = $('#gridview').prop('checked');
+            var shorten_comments = $('#shorten_comments').prop('checked');
+            var scrolling_sidebar = $('#scrolling_sidebar').prop('checked');
+            var fixed_navbar = $('#fixed_navbar').prop('checked');
 
-            localStorage["gridview"] = gridview.checked;
-            localStorage["shorten_comments"] = shorten_comments.checked;
-            localStorage["scrolling_sidebar"] = scrolling_sidebar.checked;
-            localStorage["fixed_navbar"] = fixed_navbar.checked;
+            chrome.storage.sync.set({
+                'gridview': gridview,
+                'shorten_comments' : shorten_comments,
+                'scrolling_sidebar' : scrolling_sidebar,
+                'fixed_navbar' : fixed_navbar
+            });
 
             // Update status to let user know options were saved.
-            var status = document.getElementById("status");
-            status.innerHTML = "Options Saved.";
-            setTimeout(function () {
-                status.innerHTML = "";
-            }, 750);
+            $("#status").stop(true,true).fadeIn().delay(600).fadeOut();
+
         }
 
-        // Restores select box state to saved value from localStorage.
         function restore_options() {
-            var gridview = document.getElementById('gridview');
-            var shorten_comments = document.getElementById('shorten_comments');
-            var scrolling_sidebar = document.getElementById('scrolling_sidebar');
-            var fixed_navbar = document.getElementById('fixed_navbar');
-            
-            gridview.checked = localStorage["gridview"];
-            shorten_comments.checked = localStorage["shorten_comments"];
-            scrolling_sidebar.checked = localStorage["scrolling_sidebar"];
-            fixed_navbar.checked = localStorage["fixed_navbar"];
+            chrome.storage.sync.get(function(settings){
+                if(settings.gridview === undefined) { settings.gridview = false; chrome.storage.sync.set({'gridview': settings.gridview}); }
+                if(settings.shorten_comments === undefined) { settings.shorten_comments = false; chrome.storage.sync.set({'shorten_comments', settings.shorten_comments});}
+
+            });
         }
         document.addEventListener('DOMContentLoaded', restore_options);
         document.querySelector('#save').addEventListener('click', save_options);
