1eb13b7832b5d00d860f36dfd9f15e5e4637ed50
leomoty <leomoty@gmail.com>
2014-03-10 22:59:12 -0300

    Fixed minor syntax error.

--- Google Chrome/SGPlusV2ChromeExtension/options.js
+++ Google Chrome/SGPlusV2ChromeExtension/options.js
@@ -22,8 +22,14 @@
         function restore_options() {
             chrome.storage.sync.get(function(settings){
                 if(settings.gridview === undefined) { settings.gridview = false; chrome.storage.sync.set({'gridview': settings.gridview}); }
-                if(settings.shorten_comments === undefined) { settings.shorten_comments = false; chrome.storage.sync.set({'shorten_comments', settings.shorten_comments});}
+                if(settings.shorten_comments === undefined) { settings.shorten_comments = false; chrome.storage.sync.set({'shorten_comments': settings.shorten_comments});}
+                if(settings.scrolling_sidebar === undefined) { settings.scrolling_sidebar = false; chrome.storage.sync.set({'scrolling_sidebar': settings.scrolling_sidebar}); }
+                if(settings.fixed_navbar === undefined) { settings.fixed_navbar = false; chrome.storage.sync.set({'fixed_navbar': settings.fixed_navbar}); }
 
+                $('#gridview').prop('checked', settings.gridview);
+                $('#shorten_comments').prop('checked', settings.shorten_comments);
+                $('#scrolling_sidebar').prop('checked', settings.scrolling_sidebar);
+                $('#fixed_navbar').prop('checked', settings.fixed_navbar);
             });
         }
         document.addEventListener('DOMContentLoaded', restore_options);
