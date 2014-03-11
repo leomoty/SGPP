640a1d8bac502fbf0c7b986de7ca65e13e91923a
leomoty <leomoty@gmail.com>
2014-03-10 23:26:33 -0300

    Minor mistake

--- steamgifts-plus-v2.js
+++ steamgifts-plus-v2.js
@@ -17,7 +17,7 @@ var SGPlusV2 = {
         styles.innerHTML = '.short .markdown{overflow:hidden;max-height:100px;position:relative}.less__beautify{position:absolute;width:100%;bottom:0;display:none;background:-webkit-gradient(linear,left top,left bottom,from(rgba(240,242,245,0)),to(rgba(240,242,245,1)));background:-moz-linear-gradient(top,rgba(240,242,245,0),rgba(240,242,245,1));background:linear-gradient(top,rgba(240,242,245,0),rgba(240,242,245,1));height:20px}.less__beautify.sub{background:-webkit-gradient(linear,left top,left bottom,from(rgba(243,244,247,0)),to(rgba(243,244,247,1)));background:-moz-linear-gradient(top,rgba(243,244,247,0),rgba(243,244,247,1));background:linear-gradient(top,rgba(243,244,247,0),rgba(243,244,247,1))}.short .less__beautify{display:block}.comment_more{display:none}.short .comment_more{display:block}.short .comment_less{display:none}body{margin-top:39px}header{margin-left:-25px;position:fixed;top:0;width:100%;z-index:1}.navbar_fixed{padding:0 25px}.gridview_flex{display:flex;flex-wrap:wrap;justify-content:center}';
     },
     generateGridview: function () {
-        if(!SGPlusV2.config.gridView)
+        if(SGPlusV2.config.gridView === false)
             return;
         if (SGPlusV2.location.indexOf('/giveaways/open') == -1)
             return;
@@ -36,7 +36,7 @@ var SGPlusV2 = {
         $(parent).empty().append(container);
     },
     generateScrollingSidebar: function () {
-        if(!SGPlusV2.config.sidebar)
+        if(SGPlusV2.config.sidebar === false)
             return;
         var $sidebar = $(".sidebar"),
 		$window = $(window),
@@ -55,7 +55,7 @@ var SGPlusV2 = {
         });
     },
     generateFixedNavbar: function () {
-        if(!SGPlusV2.config.fixedNavbar)
+        if(SGPlusV2.config.fixedNavbar === false)
             return;
         var nav = $('header').html();
         $('nav').remove();
@@ -70,7 +70,7 @@ var SGPlusV2 = {
         }).attr('unselectable', 'on').bind('selectstart', function () { return false; });
     },
     generateShortenedText: function () {
-        if(!SGPlusV2.config.shortenText)
+        if(SGPlusV2.config.shortenText === false)
             return;
         SGPlusV2.generateShortenedComments();
         SGPlusV2.generateShortenedDescriptions();
