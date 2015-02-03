460fab3d1769a994766b0ab8a42b73a3c27b4eb1
patetico <felipenaufal@hotmail.com>
2015-02-02 22:38:48 -0300

    Some refactoring
    Style and other minor changes to EntryCommenters.ts and re-write of
    FixedNavbar.ts

--- SGv2+/EntryCommenters.ts
+++ SGv2+/EntryCommenters.ts
@@ -4,35 +4,40 @@ module ModuleDefinition {
 
     export class EntryCommenters implements SteamGiftsModule {
         // On the giveaway entries page, add an icon next to the user names 
-        // indicating whether the user commented or not on the giveaway.
+        // indicating whether they commented or not.
 
-        private url: string;
+        private url: string = 'http://www.steamgifts.com/giveaway/';
         private cacheCompleted: boolean = false;
         private isLoading: boolean = false;
-        commenters: string[] = [];
-        private page: number = 1;
-        private maxPage: number;
+        commenters: any = {};
+        private page: number;
+        private pageStart: number = 1337; // goes down
         private elements: any = {
-            pos: $(document.createElement('i')).addClass('GAComm_pos fa fa-comment-o').attr('title', 'Commented'),
+            button: $(document.createElement('i')).addClass('giveaway__icon fa fa-comments-o').attr('title', 'Check who commented'),
+            loader: $(document.createElement('i')).addClass('giveaway__icon fa fa-refresh fa-spin').attr('title', 'loading comments').css('cursor', 'auto'),
+            pos: $(document.createElement('span')).addClass('GAComm_pos fa-stack').attr('title', 'Commented')
+                .append($(document.createElement('i')).addClass('fa fa-comment-o fa-stack-1x'))
+                .append($(document.createElement('i')).addClass('fa fa-check fa-stack-1x')),
             neg: $(document.createElement('span')).addClass('GAComm_neg fa-stack').attr('title', 'Did not comment')
                 .append($(document.createElement('i')).addClass('fa fa-comment-o fa-stack-1x'))
                 .append($(document.createElement('i')).addClass('fa fa-times fa-stack-1x')),
-            loader: $(document.createElement('i')).addClass('giveaway__icon fa fa-refresh fa-spin').attr('title', 'loading comments').css('cursor', 'auto'),
-            button: $(document.createElement('i')).addClass('giveaway__icon fa fa-comments-o').attr('title', 'Check who commented')
         };
 
         init(): void {
             var style = (
-                ".GAComm_button {text-decoration: underline; font-size: 12px}\n" +
-                ".GAComm_pos {vertical-align: super}\n" +
-                ".GAComm_neg {vertical-align: inherit}\n" +
-                ".table__column--width-fill > p {display: inline}"
+                ".GAComm_pos, .GAComm_neg {margin-left:-3px; vertical-align: inherit}\n" +
+                ".GAComm_pos > i.fa.fa-check {color: #719A47}\n" +                      // only interior
+                ".GAComm_neg > i.fa.fa-times {color: rgba(166, 93, 92, 0.85)}\n" +      // only interior
+                // ".GAComm_neg {color: rgba(166, 93, 92, 0.85)}\n" +                      // fully colored
+                // ".GAComm_pos {color: #719A47}\n" +                                      // fully colored
+                ".GAComm_pos > i.fa.fa-check, .GAComm_neg > i.fa.fa-times {font-size: 0.7em}\n"
             );
             $('<style>').attr('type', 'text/css').html(style).appendTo('head');
         }
 
         render = () => {
-            if (/.*steamgifts.com\/giveaway\/[a-zA-Z0-9]{5}\/.*?\/(entries|winners)/.test(document.URL)) {
+            // if (/.*steamgifts.com\/giveaway\/[a-zA-Z0-9]{5}\/.*?\/(entries|winners)/.test(document.URL)) {
+            if (SGPV2.location.pageKind == 'giveaway' && ['entries', 'winners'].indexOf(SGPV2.location.subpage) > -1) {
                 this.elements.button.click(this.main);
                 $('.page__heading__breadcrumbs').append(this.elements.button);
                 $('.page__heading__breadcrumbs').append(this.elements.loader.hide());
@@ -63,8 +68,10 @@ module ModuleDefinition {
 
             $('.table__rows .table__column--width-fill').each((i, el) => {
                 $('.GAComm_pos, .GAComm_neg', el).remove();
-                // if ($.trim(el.textContent) in this.commenters) {
-                if (this.commenters.indexOf($.trim(el.textContent)) > -1) {
+                var wrapper = $('p.table__column__heading', el);
+                if (wrapper.length > 0) el = wrapper[0];
+
+                if (this.commenters[el.textContent.trim()]) {
                     this.elements.pos.clone().appendTo(el);
                 } else {
                     this.elements.neg.clone().appendTo(el);
@@ -72,9 +79,10 @@ module ModuleDefinition {
             });
         }
         private getCommenters = () => {
-            this.url = /.*steamgifts.com\/giveaway\/[a-zA-Z0-9]{5}\/.*?\//.exec(document.URL)[0];
-            this.url += 'search?page=';
-            this.page = 1;
+            this.url += SGPV2.location.code + '/' + SGPV2.location.description + '/search?page='
+            // this.url = /.*steamgifts.com\/giveaway\/[a-zA-Z0-9]{5}\/.*?\//.exec(document.URL)[0];
+            // this.url += 'search?page=';
+            this.page = this.pageStart;
             this.getCommentPage();
         }
 
@@ -89,22 +97,18 @@ module ModuleDefinition {
         private handleCommentPage = (html: string) => {
             var $html = $(html);
             $('.comment__username', $html).each((i, el) => {
-                this.commenters.push(el.textContent);
+                this.commenters[el.textContent.trim()] = true;
             });
 
-            if (this.maxPage === null) {
+            if (this.page == this.pageStart) {
                 var pagination = $('a[data-page-number]', $html);
-                if (pagination.length === 0) {
-                    this.maxPage = 1;
-                } else {
-                    pagination.each((i, el) => {
-                        this.maxPage = Math.max($(el).data().pageNumber, this.maxPage);
-                    });
-                }
+                this.page = pagination.length != 0 ? pagination.last().data().pageNumber : 1
             }
 
-            if (++this.page <= this.maxPage) this.getCommentPage();
-            else this.cacheCompleted = true;
+            if (--this.page > 0)
+                this.getCommentPage();
+            else
+                this.cacheCompleted = true;
 
         }
 
