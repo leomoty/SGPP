542740941a184cdc79fb90b9b261aecd4bb4de0e
leomoty <leomoty@gmail.com>
2015-02-06 21:21:30 -0200

    Removes unneeded classes

--- SGv2+/FixedNavbar.ts
+++ SGv2+/FixedNavbar.ts
@@ -7,14 +7,13 @@ module ModuleDefinition{
         style = "";
 
         init(): void {
-            var style = ".body {margin-top: 39px}" +
-                ".header {position: fixed; top: 0; width: 100%; z-index: 100}";
+            var style = "body {margin-top: 39px}" +
+                "header {position: fixed; top: 0; width: 100%; z-index: 100}";
             $('<style>').attr('type', 'text/css').html(style).appendTo('head');
         }
 
         render(): void {
-            $('body').addClass('body');
-            $('header').addClass('header');
+
         }
 
         name(): string {
