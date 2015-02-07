cd06d7c5a8bdf07e979bc1d56dfe723335b7a6f6
leomoty <leomoty@gmail.com>
2015-02-06 22:16:11 -0200

    Adds important tag on both Footer and Navbar
    Firefox seems to not pick this rule as active without !important

--- SGv2+/FixedFooter.ts
+++ SGv2+/FixedFooter.ts
@@ -7,7 +7,7 @@ module ModuleDefinition{
         style = "";
 
         init(): void {
-            var style = "body {margin-bottom: 45px}" +
+            var style = "body {margin-bottom: 45px !important}" +
                 ".footer__outer-wrap {position: fixed; bottom: 0; width: 100%; background-color: #95a4c0}";
             $('<style>').attr('type', 'text/css').html(style).appendTo('head');
         }
--- SGv2+/FixedNavbar.ts
+++ SGv2+/FixedNavbar.ts
@@ -7,13 +7,12 @@ module ModuleDefinition{
         style = "";
 
         init(): void {
-            var style = "body {margin-top: 39px}" +
+            var style = "body {margin-top: 39px !important }" +
                 "header {position: fixed; top: 0; width: 100%; z-index: 100}";
             $('<style>').attr('type', 'text/css').html(style).appendTo('head');
         }
 
         render(): void {
-
         }
 
         name(): string {
