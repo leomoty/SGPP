40600877c5ee3395142e61bcb656e2cad08d2222
leomoty <leomoty@gmail.com>
2015-02-13 23:45:20 -0200

    Refactor LocalStorage from Settings.
    It is exposed as SGPP.storage now

---
+++ SGv2+/LocalStorage.ts
@@ -0,0 +1,19 @@
+﻿module ModuleDefinition {
+
+    export class LocalStorage {
+        private _LSPrefix: string = "SGPP_";
+
+        containsItem = (key: string): boolean => {
+            return localStorage.getItem(this._LSPrefix + key) != null;
+        }
+
+        getItem = (key: string): any => {
+            return localStorage.getItem(this._LSPrefix + key);
+        }
+
+        setItem = (key: string, value: any): void => {
+            localStorage.setItem(this._LSPrefix + key, value);
+        }
+    }
+
+}
