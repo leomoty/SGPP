76c63a6a961d53096477f88181f7662e15da2a45
patetico <felipenaufal@hotmail.com>
2015-02-15 06:24:54 -0300

    Correct class names on FixedFooter

--- SGPP/Modules/FixedFooter.ts
+++ SGPP/Modules/FixedFooter.ts
@@ -5,16 +5,16 @@ module ModuleDefinition{
     export class FixedFooter implements SteamGiftsModule {
 
         style = "body.SGPP_FixedFooter {padding-bottom: 45px}\n" +
-            ".footer__outer-wrap.SGPP_FixedFooter_innerWrap {padding: 15px 0px; z-index: 100; bottom: 0px; position: fixed; width: 100%; background: inherit}\n" +
-            ".footer__inner-wrap.SGPP_FixedFooter_outerWrap {margin: 0px 25px}\n";
+            ".footer__outer-wrap.SGPP_FixedFooter_outerWrap {padding: 15px 0px; z-index: 100; bottom: 0px; position: fixed; width: 100%; background: inherit}\n" +
+            ".footer__inner-wrap.SGPP_FixedFooter_innerWrap {margin: 0px 25px}\n";
 
         init(): void {
         }
 
         render(): void {
             $('body').addClass('SGPP_FixedFooter');
-            $('.footer__outer-wrap').addClass('SGPP_FixedFooter_innerWrap');
-            $('.inner-wrap').addClass('SGPP_FixedFooter_outerWrap');
+            $('.footer__outer-wrap').addClass('SGPP_FixedFooter_outerWrap');
+            $('.footer__inner-wrap').addClass('SGPP_FixedFooter_innerWrap');
         }
 
         name(): string {
