21892950a99d5291322336f066d97e0107da7c7d
leomoty <leomoty@gmail.com>
2015-02-16 07:22:28 -0200

    Adds @include to monkeyheader

--- Monkeyheader.js
+++ Monkeyheader.js
@@ -5,6 +5,7 @@
 // @description     SG++ for Steamgifts.com
 // @author          Leomoty
 // @match           http://www.steamgifts.com/*
+// @include         http://www.steamgifts.com/*
 // @run-at          document-end
 // @require         http://code.jquery.com/jquery-2.1.3.min.js
 // @require         https://raw.githubusercontent.com/dinbror/bpopup/master/jquery.bpopup.min.js
