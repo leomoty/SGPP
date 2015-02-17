853a061825b8ce1e88e227d3d9e5e34769c7a2ed
leomoty <leomoty@gmail.com>
2015-02-16 22:06:46 -0200

    Compiles 0.2.3 beta

--- Monkeyheader.js
+++ Monkeyheader.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name            Steamgifts++ Dev
+// @name            Steamgifts++
 // @namespace       https://github.com/leomoty/SGPP
-// @version         0.2.2 beta
+// @version         0.2.3 beta
 // @description     SG++ for Steamgifts.com
 // @author          Leomoty
 // @match           http://www.steamgifts.com/*
-// @include         http://www.steamgifts.com/*
 // @run-at          document-end
+// @downloadURL     https://raw.githubusercontent.com/leomoty/SGPP/master/SGPP.user.js
+// @updateURL       https://raw.githubusercontent.com/leomoty/SGPP/master/SGPP.meta.js
 // @require         http://code.jquery.com/jquery-2.1.3.min.js
 // @require         https://raw.githubusercontent.com/dinbror/bpopup/master/jquery.bpopup.min.js
 // @grant           GM_addStyle
