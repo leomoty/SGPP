2c0a431fe517ffa04c49508abf0fb326480666a8
patetico <felipenaufal@hotmail.com>
2015-02-19 05:22:12 -0300

    Rewrite CommentAndEnter

--- SGPP/Modules/CommentAndEnter.ts
+++ SGPP/Modules/CommentAndEnter.ts
@@ -10,48 +10,53 @@ module ModuleDefinition{
 
         shouldRun = (location: SGLocation) => location.pageKind == 'giveaway' && location.subpage == '';
 
-        style = ".comment_submit{margin-bottom:0!important}";
+        style = "div.SGPP_CommAndEnter {margin: 0 0 0 5px}\n";
 
         init = () => {
         
         }
 
         render = () => {
-        	var submit = $('.js__submit-form');
-            submit.before('<div class="sidebar__entry-insert comment_submit is-hidden">Comment and Enter</div>');
-            submit.before('<div class="sidebar__entry-loading is-disabled is-hidden comment_submit"><i class="fa fa-refresh fa-spin"></i> Please wait...</div>');
-            var insert = $('.sidebar .sidebar__entry-insert');
-            var remove = $('.sidebar .sidebar__entry-delete');
-            var button = $('.comment_submit.sidebar__entry-insert');
-            var loading = $('.comment_submit.sidebar__entry-loading');
+            var enterGa = $('.sidebar .sidebar__entry-insert');
+            if (enterGa.length === 0) return; // assume the giveaway is unavailable
+            var leaveGa = $('.sidebar .sidebar__entry-delete');
+            var sidebarLoading = $('.sidebar .sidebar__entry-loading');
 
-            if(!insert.hasClass('is-hidden'))
-                button.removeClass('is-hidden');
-            insert.on('click', function() {
-                button.addClass('is-hidden');
-                loading.removeClass('is-hidden');
-            });
-            remove.on('click', function() {
-                loading.addClass('is-hidden');
-                button.removeClass('is-hidden');
-            });
-            button.on('click', function() {
-                insert.click();
-                button.addClass('is-hidden');
-                loading.removeClass('is-hidden');
+            var submitBtn = $('.js__submit-form');
+            var commLoading = sidebarLoading.clone().addClass('SGPP_CommAndEnter is-hidden').insertAfter(submitBtn);
+            var commAndEnter = $('<div class="sidebar__entry-insert SGPP_CommAndEnter is-hidden">Submit + Enter</div>').insertAfter(submitBtn);
 
-                var observer = new MutationObserver(function (mutations) {
-                    mutations.forEach(function (mutation) {
-                        if (!$(mutation.target).hasClass('is-hidden')) {
-                            submit.closest("input[name=do]").val("comment_new");
-                            submit.closest("form").submit()
-                        }
-                    });
-                });
+            commAndEnter.toggleClass('is-hidden', enterGa.hasClass('is-hidden'));
 
-                observer.observe(remove[0], { attributes: true, attributeFilter: ['class'] });
+            var enterObserver = new MutationObserver(function(mutations) {
+                for (var i=0; i < mutations.length; i++) {
+                    var el = mutations[i].target;
+                    if (el == enterGa[0])
+                        commAndEnter.toggleClass('is-hidden', enterGa.hasClass('is-hidden'));
+                    else if (el == sidebarLoading[0])
+                        commLoading.toggleClass('is-hidden', sidebarLoading.hasClass('is-hidden'));
+                }
+            })
 
-            });
+            var opt = {attributes: true, attributeFilter: ['class']};
+            enterObserver.observe(enterGa[0], opt);
+            enterObserver.observe(sidebarLoading[0], opt);
+
+            commAndEnter.click(function(e) {
+                commAndEnter.addClass('is-hidden');
+                commLoading.removeClass('is-hidden');
+                $('textarea.description').prop('disabled', true);
+                enterGa.click();
+
+                var leaveObserver = new MutationObserver(function(mutations) {
+                    if (!leaveGa.hasClass('is-hidden')) {
+                        submitBtn.closest("input[name=do]").val("comment_new");
+                        submitBtn.closest("form").submit();
+                    }
+                })
+                leaveObserver.observe(leaveGa[0], opt);
+            })
+
         }
 
     }
