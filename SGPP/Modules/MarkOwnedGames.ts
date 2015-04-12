344108e7affa3765113be520afe7dd1d146b15c6
Niko <madjoki@gmail.com>
2015-04-12 20:59:47 +0300

    Hide entry button for owned apps, that steamgifts doesn't detect (DLC).

--- SGPP/Modules/MarkOwnedGames.ts
+++ SGPP/Modules/MarkOwnedGames.ts
@@ -15,11 +15,66 @@ module ModuleDefinition {
         init(): void {
         }
 
+        ownsApp(appid: number): boolean {
+            return this.userdata.rgOwnedApps.indexOf(appid) !== -1;
+        }
+
+        ownsPackage(packageid: number): boolean {
+            return this.userdata.rgOwnedPackages.indexOf(packageid) !== -1;
+        }
+
+        ignoresApp(appid: number): boolean {
+            return this.userdata.rgIgnoredApps.indexOf(appid) !== -1;
+        }
+
+        ignoresPackage(packageid: number): boolean {
+            return this.userdata.rgIgnoredPackages.indexOf(packageid) !== -1;
+        }
+
+        isWishlisted(appid: number): boolean {
+            return this.userdata.rgWishlist.indexOf(appid) !== -1;
+        }
+
         render(): void {
             if (!SGPP.storage.containsItem("steam_userdata") || !SGPP.storage.containsItem("steam_userdata_date") || SGPP.storage.getItem("steam_userdata_date") < (Date.now() - 12 * 60 * 60 * 1000)) {
                 this.refreshGamesFromSteam();
-            } else {
-                this.userdata = JSON.parse(SGPP.storage.getItem("steam_userdata"));
+            }
+
+            if (SGPP.storage.containsItem("steam_userdata")) {
+                this.userdata = SGPP.storage.getItem("steam_userdata");
+            }
+
+            console.log(this.userdata);
+
+            if (SGPP.location.pageKind == 'giveaway') {
+                var link = $('a.global__image-outer-wrap--game-large').first().attr('href');
+                var owned = false;
+                var ignored = false;
+                var appid = -1;
+
+                var m = link.match(/\/app\/(\d+)\//);
+
+                if (m) {
+                    appid = parseInt(m[1]);
+                    owned = this.ownsApp(appid);
+                    ignored = this.ignoresApp(appid);
+
+                    console.log(owned);
+                    console.log(ignored);
+                    console.log(appid);
+                }
+
+                var sidebar = $('.sidebar').last();
+
+                if (owned) {
+                    if ($('.sidebar__entry-insert').length != 0) {
+                        $('.sidebar__entry-insert').before('<div class="sidebar__error is-disabled"><i class="fa fa-exclamation-circle"></i> Exists in Account</div>');
+                        $('.sidebar__entry-insert').hide();
+                    }
+                }
+                if (ignored) {
+                    sidebar.prepend('<div>Ignored</div>');
+                }
             }
         }
 
@@ -35,7 +90,7 @@ module ModuleDefinition {
                         alert('You are not logged in to Steam.');
                     }
 
-                    SGPP.storage.setItem("steam_usedata", userdata);
+                    SGPP.storage.setItem("steam_userdata", userdata);
                     SGPP.storage.setItem("steam_userdata_date", Date.now());
 
                     this.userdata = userdata;
--- SGPP/SteamgiftsPlusV2.js
+++ SGPP/SteamgiftsPlusV2.js
@@ -1100,12 +1100,53 @@ var ModuleDefinition;
         };
         MarkOwnedGames.prototype.init = function () {
         };
+        MarkOwnedGames.prototype.ownsApp = function (appid) {
+            return this.userdata.rgOwnedApps.indexOf(appid) !== -1;
+        };
+        MarkOwnedGames.prototype.ownsPackage = function (packageid) {
+            return this.userdata.rgOwnedPackages.indexOf(packageid) !== -1;
+        };
+        MarkOwnedGames.prototype.ignoresApp = function (appid) {
+            return this.userdata.rgIgnoredApps.indexOf(appid) !== -1;
+        };
+        MarkOwnedGames.prototype.ignoresPackage = function (packageid) {
+            return this.userdata.rgIgnoredPackages.indexOf(packageid) !== -1;
+        };
+        MarkOwnedGames.prototype.isWishlisted = function (appid) {
+            return this.userdata.rgWishlist.indexOf(appid) !== -1;
+        };
         MarkOwnedGames.prototype.render = function () {
             if (!SGPP.storage.containsItem("steam_userdata") || !SGPP.storage.containsItem("steam_userdata_date") || SGPP.storage.getItem("steam_userdata_date") < (Date.now() - 12 * 60 * 60 * 1000)) {
                 this.refreshGamesFromSteam();
             }
-            else {
-                this.userdata = JSON.parse(SGPP.storage.getItem("steam_userdata"));
+            if (SGPP.storage.containsItem("steam_userdata")) {
+                this.userdata = SGPP.storage.getItem("steam_userdata");
+            }
+            console.log(this.userdata);
+            if (SGPP.location.pageKind == 'giveaway') {
+                var link = $('a.global__image-outer-wrap--game-large').first().attr('href');
+                var owned = false;
+                var ignored = false;
+                var appid = -1;
+                var m = link.match(/\/app\/(\d+)\//);
+                if (m) {
+                    appid = parseInt(m[1]);
+                    owned = this.ownsApp(appid);
+                    ignored = this.ignoresApp(appid);
+                    console.log(owned);
+                    console.log(ignored);
+                    console.log(appid);
+                }
+                var sidebar = $('.sidebar').last();
+                if (owned) {
+                    if ($('.sidebar__entry-insert').length != 0) {
+                        $('.sidebar__entry-insert').before('<div class="sidebar__error is-disabled"><i class="fa fa-exclamation-circle"></i> Exists in Account</div>');
+                        $('.sidebar__entry-insert').hide();
+                    }
+                }
+                if (ignored) {
+                    sidebar.prepend('<div>Ignored</div>');
+                }
             }
         };
         MarkOwnedGames.prototype.refreshGamesFromSteam = function () {
@@ -1118,7 +1159,7 @@ var ModuleDefinition;
                     if (userdata.rgOwnedApps.length == 0) {
                         alert('You are not logged in to Steam.');
                     }
-                    SGPP.storage.setItem("steam_usedata", userdata);
+                    SGPP.storage.setItem("steam_userdata", userdata);
                     SGPP.storage.setItem("steam_userdata_date", Date.now());
                     _this.userdata = userdata;
                 }
