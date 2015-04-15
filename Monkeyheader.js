8522692a0939d81e9c3ba9b6f20617eb01b1603a
Niko <madjoki@gmail.com>
2015-04-16 00:44:36 +0300

    Change settings url

--- Monkeyheader.js
+++ Monkeyheader.js
@@ -10,7 +10,7 @@
 // @require         http://code.jquery.com/jquery-2.1.3.min.js
 // @require         https://raw.githubusercontent.com/dinbror/bpopup/master/jquery.bpopup.min.js
 // @resource head   https://raw.githubusercontent.com/leomoty/SGPPSettings/master/head.html
-// @resource settings   https://raw.githubusercontent.com/nikop/SGPPSettings/gamesfilter/settings.html
+// @resource settings    https://raw.githubusercontent.com/leomoty/SGPPSettings/master/settings.html
 // @grant           GM_addStyle
 // @grant           GM_getResourceText
 // @grant			GM_xmlhttpRequest
